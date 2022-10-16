6098431f7b6e62a9f255f362c369dd31a813197d
DanielChevez <cali1511a.com@gmail.com>
2022-10-16 11:26:13 -0600

    Agregando Funcion Ayuda

--- src/main/java/com/una/proyecto1/Compilador.java
+++ src/main/java/com/una/proyecto1/Compilador.java
@@ -22,120 +22,167 @@ public class Compilador extends javax.swing.JFrame {
     int num2;
 
     //Expresiones regulares
-    String palabras[] = {"SUMA", "RESTA", "MULTIPLICACION", "DIVISION", "INT", "IMPRIMIR", "ELEVAR", "RAIZ2", "RAIZ3", "FIBONACCI"}; //conjunto de palabras reservadas
+    String palabras[] = {"SUMA", "RESTA", "MULTIPLICACION", "DIVISION", "INT", "IMPRIMIR", "ELEVAR", "RAIZ2", "RAIZ3", "FIBONACCI", "AYUDA"}; //conjunto de palabras reservadas
     boolean stopSystem = false; //Se usa en caso de que ocurra un error en la funcion sintaxis
     HashMap<String, Integer> vars = new HashMap<>();
 
     public void sintax(String pSentencia, boolean pActionSwitch) {
         boolean activeSwitch = pActionSwitch;
         byte result = -2;
-        
+
         int value1 = 0;
         int value2 = 0;
-        String mensaje = "",text = " ";
+        String mensaje = "", text = " ";
         String[] array = new String[3];
-
-       // byte saveVar = 0;
         array = pSentencia.split(" ");
-        System.out.println(array.length);
-        for (byte i = 0; i < this.palabras.length; i++) {//Se recorre buscando la funcion
+        
+        for (byte i = 0; i < this.palabras.length; i++) {//Se recorre buscando la funcion invocada por el usuario
             if (array[0].matches(this.palabras[i])) {
                 result = i;
                 break;
             }
         }
         if (result >= 0) {
-            if (result == 4) { //En caso de que sea una varible                                
-                //saveVar = this.sintaxVariable(pSentencia);
-                if(this.sintaxVariable(pSentencia) != 1){ //SI es diferente de 1 sigifica que hubo un error => (duplicidad,no existe,guarda letras)
+            if (result == 4) { //En caso de que sea una varible                                                
+                if (this.sintaxVariable(pSentencia) != 1) { //SI es diferente de 1 sigifica que hubo un error => (duplicidad,no existe,guarda letras)
                     return;
-                }                               
+                }
             }//Fin Verificacion Variable
             else if (result == 5) {//Se verifica la funcion IMPRIMIR
-                array = pSentencia.split(" ");
-                System.out.println(array.length);
+                array = pSentencia.split(" ");                
                 text = this.Error.getText();
+                
                 for (int i = 1; i < array.length; i++) { //Se toma el mensaje
                     mensaje = mensaje + " " + array[i];
                 }
-                this.Error.setText(text + "\n" +mensaje);
-                System.out.println(mensaje);
+                this.Error.setText(text + "\n" + mensaje);
                 
+
                 return;
-                
+
             }//Fin Imprimir
+
             
-            //Como las funciones se van a trabajar con 2 valores por eso lo hice asi
             //Se procede a verificar si son numeros o variables
             //Puede que el array venga de 2 (caso de las funciones de raiz) o de 3
-            if(array.length ==2){
+            if (array.length == 2) {
                 value1 = getValue(array[1]);
-            } else{
+            } else if (array.length == 3) {
                 value1 = getValue(array[1]);
                 value2 = getValue(array[2]);
             }
-            
-            
-            if(value1 == 'n' || value2 =='n'){//Si alguno de los valores no es valido         
+
+            if (value1 == 'n' || value2 == 'n') {//Si alguno de los valores no es valido         
                 activeSwitch = false;//Se desactiva el switch en caso de que la variable o numero no sea valido
                 return;
-            }else{   
-                if(!activeSwitch){//Se activa el Swicth si la funcion es llamada desde el boton compilar
-                this.Error.setText("Compilacion Exitosa");
+            } else {
+                if (!activeSwitch) {//Se activa el Swicth si la funcion es llamada desde el boton compilar
+                    this.Error.setText("Compilacion Exitosa");
                 }
-            }            
-            
-            if(activeSwitch){//Se activa el Swicth, ya que la funcion es llamada por "CompilaryEjecutar"
-                fnSwitch(result,value1,value2,mensaje);                
             }
-            
+
+            if (activeSwitch) {//Se activa el Swicth, ya que la funcion es llamada por "CompilaryEjecutar"
+                fnSwitch(result, value1, value2, mensaje);
+            }
+
         } else {
             this.Error.setText("Error de Compilacion");
             this.stopSystem = true;
         }
     }
-    public int getValue(String pVariable){
+
+    public int getValue(String pVariable) {
         boolean numeric = pVariable.matches("\\d*");
         if (numeric) {
-                return Integer.parseInt(pVariable);
+            return Integer.parseInt(pVariable);
 
+        } else {
+            if (this.vars.containsKey(pVariable)) {
+                return this.vars.get(pVariable);
             } else {
-                if (this.vars.containsKey(pVariable)) {
-                    return this.vars.get(pVariable);
-                }else{
-                    this.Error.setText("Errror!!! La variable: "+pVariable+" no existe.");
-                    this.stopSystem = true;
-                }
+                this.Error.setText("Errror!!! La variable: " + pVariable + " no existe.");
+                this.stopSystem = true;
             }
+        }
         return 'n';
     }
-    
-    public int fibo(int n){
-         if (n == 0) {
-            return 0;   
+
+    public int fibo(int n) {
+        if (n == 0) {
+            return 0;
         } else if (n == 1) {
             return 1;
         } else {
             //Hago la suma
             return fibo(n - 1) + fibo(n - 2);
         }
- 
+
     }
-    
-    public void fnSwitch(byte pPoss, int value1, int value2,String pMensaje) {
+
+    public void ayuda() {
+        String text = " ";
+
+        this.Error.setText("\t\tINFORMACIÓN DEL SISTEMA\n\n");
+
+        for (byte i = 0; i < this.palabras.length; i++) {
+            text = this.Error.getText();
+            switch (i) {
+                case 0:
+                    this.Error.setText(text +"\n"+i+ "=> SUMA X1 X2;");
+                    break;
+                case 1:
+                    this.Error.setText(text +"\n"+i+ "=> RESTA X1 X2;");
+                    break;
+                case 2:
+                    this.Error.setText(text +"\n"+i+ "=> MULTIPLICACION X1 X2;");
+                    break;
+                case 3:
+                    this.Error.setText(text+"\n"+i+ "=> DIVISION X1 X2;");
+                    break;
+                case 4:
+                    this.Error.setText(text +"\n"+i+ "=> Declaracion Variable: INT NOMBRE X1;");
+                    break;
+                case 5:
+                    this.Error.setText(text +"\n"+i+ "=> IMPRIMIR TEXTO;");
+                    break;
+                case 6:
+                    this.Error.setText(text +"\n"+i+ "=> ELEVAR X1 X2;");
+                    break;
+                case 7:
+                    this.Error.setText(text +"\n"+i+ "=> Raiz Cuadrada: RAIZ2 X1;");
+                    break;
+                case 8:
+                    this.Error.setText(text +"\n"+i+ "=> Raiz Cubica: RAIZ3 X1;");
+                    break;
+                case 9:
+                    this.Error.setText(text +"\n"+i+ "=> FIBONACCI X1;");
+                    break;
+                case 10:
+                    this.Error.setText(text +"\n"+i+ "=> Informacion del Sistema:AYUDA;");
+                    break;
+                default:
+                    break;
+            }
+            //String palabras[] = {"ELEVAR", "RAIZ2", "RAIZ3", "FIBONACCI","?"}; //conjunto de palabras reservadas
+
+        }
+
+    }
+
+    public void fnSwitch(byte pPoss, int value1, int value2, String pMensaje) {
         String text = this.Error.getText();
-            int resultado = 0;
+        int resultado = 0;
         switch (pPoss) {
             case 0://Suma
-               resultado = value1 + value2;
+                resultado = value1 + value2;
                 this.Error.setText(text + "\n" + "El resultado de la suma es: " + resultado);
-                 
+
                 break;
             case 1://Resta
-                    resultado = value1 - value2;
+                resultado = value1 - value2;
                 this.Error.setText(text + "\n" + "El resultado de la resta es: " + resultado);
                 break;
-                
+
             case 2://Multiplicacion
                 resultado = value1 * value2;
                 this.Error.setText(text + "\n" + "El resultado de la multiplicacion es: " + resultado);
@@ -145,17 +192,17 @@ public class Compilador extends javax.swing.JFrame {
                 this.Error.setText(text + "\n" + "El resultado de la division es: " + resultado);
                 break;
             case 5://Imprimir
-                 this.Error.setText(text + "\n" + pMensaje);               
+                this.Error.setText(text + "\n" + pMensaje);
                 break;
             case 6: //Potencia
                 resultado = 1;
                 System.out.println(value1);
                 System.out.println(value2);
-                while(value2 != 0){
+                while (value2 != 0) {
                     resultado = resultado * value1;
                     value2--;
                 }
-                 this.Error.setText(text + "\n" + "El resultado de la potencia es: " + resultado);
+                this.Error.setText(text + "\n" + "El resultado de la potencia es: " + resultado);
                 break;
             case 7: //Raiz cuadrada
                 resultado = (int) Math.sqrt(value1);
@@ -163,14 +210,18 @@ public class Compilador extends javax.swing.JFrame {
                 break;
             case 8: //Raiz cubica
                 float val1 = value1;
-                double res2; 
-                res2 = Math.pow(val1, (double) 1/3);
+                double res2;
+                res2 = Math.pow(val1, (double) 1 / 3);
                 this.Error.setText(text + "\n" + "El resultado de la raiz cubica es: " + res2);
                 break;
             case 9: //Fibonacci
                 resultado = fibo(value1);
-                this.Error.setText(text + "\n" + "Fibonacci de "+value1+" es: " +resultado);
+                this.Error.setText(text + "\n" + "Fibonacci de " + value1 + " es: " + resultado);
                 break;
+            case 10: //Ayuda
+                this.ayuda();
+                break;
+
         }
 
     }
@@ -181,7 +232,7 @@ public class Compilador extends javax.swing.JFrame {
         boolean numeric = false;
         array = pSentencia.split(" ");
         numeric = array[2].matches("\\d*");
-        
+
         //Vertifica que el nombre de la variable no se encuentre en el arreglo de palabras reservadas
         for (byte j = 0; j < this.palabras.length; j++) {
             insert = this.palabras[j].equals(array[1]);//True == Encuentra coincidencias 
@@ -431,15 +482,15 @@ public class Compilador extends javax.swing.JFrame {
         this.stopSystem = false;
         StringTokenizer tokens = new StringTokenizer(texto, ";\n\r");
         String sentencia = "";
-        
 
         while (tokens.hasMoreTokens()) {
 
             sentencia = tokens.nextToken();
-            if(!stopSystem){
-             sintax(sentencia, false);            
-            }else
+            if (!stopSystem) {
+                sintax(sentencia, false);
+            } else {
                 return;
+            }
 
         }
     }//GEN-LAST:event_CompilarActionPerformed
@@ -456,13 +507,14 @@ public class Compilador extends javax.swing.JFrame {
         vars.clear(); //limpiamos por si la persona quiere compilar y ejecutar varias veces
         String texto = this.txtATexto1.getText();
         StringTokenizer tokens = new StringTokenizer(texto, ";\n\r");
-        String sentencia = "";        
+        String sentencia = "";
         while (tokens.hasMoreTokens()) {
             sentencia = tokens.nextToken();
-           if(!stopSystem){
-             sintax(sentencia, true);//Como se ejecuta se manda  true para que se ejecuten las funciones      
-            }else
+            if (!stopSystem) {
+                sintax(sentencia, true);//Como se ejecuta se manda  true para que se ejecuten las funciones      
+            } else {
                 return;
+            }
         }
     }//GEN-LAST:event_compejecutarActionPerformed
 
